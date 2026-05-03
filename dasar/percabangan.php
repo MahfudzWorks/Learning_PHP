2fcd7215a45db0a2a985df1a08d5d3c56c1f5fbe
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-05-02 22:45:12 -0700

    commit Percabangan IF ELSE

---
+++ dasar/percabangan.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "<h1>Belajar Percabangan PHP</h1>";
+
+// IF ELSE
+
+echo "<h2>If Else</h2>";
+
+$nilai = 90;
+
+if ($nilai >= 75) {
+    echo "Nilai: $nilai <br>";
+    echo "Status: Lulus <br>";
+} else {
+    echo "Nilai: $nilai <br>";
+    echo "Status: Tidak Lulus <br>";
+}
+
+?>
