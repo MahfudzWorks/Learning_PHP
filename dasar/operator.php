8193e9bda8ff4b9772382c9ac465bde3ebd2c579
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-05-02 18:17:30 -0700

    commit Operator Dasar

---
+++ dasar/operator.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<h1>Belajar Operator PHP</h1>";
+
+// Operator Aritmatika
+
+echo "<h2>Operator Aritmatika</h2>";
+
+$a = 10;
+$b = 5;
+
+echo "a = $a, b = $b <br>";
+echo "Penjumlahan: ". ($a + $b) ."<br>";
+echo "Pengurangan: ". ($a - $b) ."<br>";
+echo "Perkalian: ". ($a * $b) ."<br>";
+echo "Pembagian: ". ($a / $b) ."<br>";
+echo "Modulus (sisa bagi): ". ($a % $b) ."<br>";
+?>
