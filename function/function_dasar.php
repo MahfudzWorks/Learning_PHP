ea7ce8d2861f961f2d6791d77abef48455bb1d2e
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-05-04 16:11:07 -0700

    function dasar

---
+++ function/function_dasar.php
@@ -0,0 +1,15 @@
+<?php
+
+echo "<h1>Belajar Function Dasar PHP</h1>";
+
+// FUNCTION TANPA PARAMETER
+
+function salam() {
+    echo "Halo, selamat belajar PHP! <br>";
+}
+
+echo "<h2>Function Tanpa Parameter</h2>";
+salam();
+salam(); 
+
+?>
