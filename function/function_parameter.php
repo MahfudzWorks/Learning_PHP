cfad5fd7ee8bbbcc31a2e756337df679040d5e2f
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-05-06 05:21:17 -0700

    commit Function Parameter

---
+++ function/function_parameter.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "<h1>Belajar Function dengan Parameter<h1>";
+
+//FUNCTION DENGAN PARAMETER
+
+function salam($nama) {
+    echo "Halo . $nama! <br>";
+}
+
+echo "<h2>Function dengan Parameter</h2>";
+salam("Mahfudz");
+salam("Budi");
+salam("Siti");
+
+?>
