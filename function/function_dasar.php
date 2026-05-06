5f36ab492e2332152035369a4e4d882f03f88071
Mahfudz Alfanani Syaviqi <mahfudzworksss@gmail.com>
2026-05-06 05:14:35 -0700

    commit Function dasar 2

--- function/function_dasar.php
+++ function/function_dasar.php
@@ -10,6 +10,27 @@ function salam() {
 
 echo "<h2>Function Tanpa Parameter</h2>";
 salam();
-salam(); 
+salam();
 
+// FUNCTION DENGAN RETURN
+
+function hitungLuasPersegi() {
+    $sisi = 5;
+    return $sisi * $sisi;
+}
+
+echo "<h2>Function dengan Return</h2>";
+echo "Luas persegi: " . hitungLuasPersegi() . "<br>";
+
+// FUNCTION DENGAN VARIABEL GLOBAL
+
+$nama = "Mahfudz";
+
+function tampilNama() {
+    global $nama;
+    echo "Nama saya: $nama <br>";
+}
+
+echo "<h2>Function dengan Global</h2>";
+tampilNama();
 ?>
